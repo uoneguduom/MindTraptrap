02f7279aa107aa95df7c50bd49d02e1586264c88
uoneguduom <g.voineau@icloud.com>
2025-03-12 15:31:23 +0100

    update

--- js/room.js
+++ js/room.js
@@ -1,4 +1,5 @@
-const Room = document.querySelector("div")
-const idRoom = Room.id
-localStorage.setItem("nbRoom", idRoom)
-window.location.replace("");
+const Room = document.querySelector("div");
+const idRoom = Room.id;
+localStorage.setItem("nbRoom", idRoom);
+// window.location.replace("");
+document.location.href="https://uoneguduom.github.io/MindTraptrap/html/yourClue.html"
