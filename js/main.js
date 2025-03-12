b24e04897594475d5d70a35a8e30b54be26b08e3
uoneguduom <g.voineau@icloud.com>
2025-03-12 10:02:59 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+"use-strict"
+
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(`Bouton cliqué : ${button.id}`);
+    localStorage.setItem("playerNb", button.id)
+    choiceMade()
+  });
+});
+
+const choiceMade = () => {
+  const content = document.querySelectorAll(".content");
+  
+  content.forEach(element => {
+    element.remove();
+  });
+};
