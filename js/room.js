63ec309f10bea4bb6645da477114c0abe763607f
uoneguduom <g.voineau@icloud.com>
2025-03-12 13:23:13 +0100

    update

--- js/room.js
+++ js/room.js
@@ -1,5 +1,6 @@
 const Room = document.querySelector("div")
 const idRoom = Room.id
+console.log("Before Local Storage")
 localStorage.setItem("nbRoom", idRoom)
+console.log("After Local Storage")
 window.location.replace("html/yourClue.html");
-
